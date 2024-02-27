b92193e3af8eef8ed81d7fd7b827b5685fd3058d
leila <143362178+1e1104k4@users.noreply.github.com>
2024-02-26 20:52:25 -0600

    Initial Commit

---
+++ FillTres/FillTresApp.swift
@@ -0,0 +1,17 @@
+//
+//  FillTresApp.swift
+//  FillTres
+//
+//  Created by Leila on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct FillTresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
