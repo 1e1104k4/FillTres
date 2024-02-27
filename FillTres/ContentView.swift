5fcda3e55de4aad06de2872498d962f88133cb42
leila <143362178+1e1104k4@users.noreply.github.com>
2024-02-26 21:45:37 -0600

    importing image into SwiftUI using PhotosPicker

--- FillTres/ContentView.swift
+++ FillTres/ContentView.swift
@@ -5,23 +5,30 @@
 //  Created by Leila on 2/26/24.
 //
 
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
     @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
+    @State private var selectedItem: PhotosPickerItem?
+    
     var body: some View {
         NavigationStack {
             VStack {
                 Spacer()
                 
-                if let processedImage {
-                    processedImage
-                        .resizable()
-                        .scaledToFit()
-                } else {
-                    ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                    }
                 }
+                .buttonStyle(.plain)
+                .onChange(of: selectedItem, loadImage)
                 
                 Spacer()
                 
@@ -45,6 +52,15 @@ struct ContentView: View {
     
     func changeFilter() {
         
+    }
+    
+    func loadImage() {
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+            guard let inputData = UIImage(data: imageData) else { return }
+            
+            // .....
+        }
     }
 }
 
