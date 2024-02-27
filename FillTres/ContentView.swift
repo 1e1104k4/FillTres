bce001564100a25c524edf04e6d9b81f55f5eefa
leila <143362178+1e1104k4@users.noreply.github.com>
2024-02-26 21:26:26 -0600

    basic ui layout

--- FillTres/ContentView.swift
+++ FillTres/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                if let processedImage {
+                    processedImage
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+                    
+                    Spacer()
+                    
+                    // share pic
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("FillTres")
         }
-        .padding()
+    }
+    
+    func changeFilter() {
+        
     }
 }
 
